944b9a43fed71179ff37e3a52016520b45bc06b1
dmsn002 <128927066+dmsn002@users.noreply.github.com>
2025-03-28 20:50:16 +0800

    Update diy.sh

--- N1-LEDE/diy.sh
+++ N1-LEDE/diy.sh
@@ -11,20 +11,34 @@ function git_sparse_clone() {
 
 # Add packages
 #添加科学上网源
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
+#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
 git clone -b 18.06 --single-branch --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone -b 18.06 --single-branch --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
-git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go package/ddnsgo
+#git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go package/ddnsgo
 #git clone --depth=1 https://github.com/sirpdboy/NetSpeedTest package/NetSpeedTest
 
-git clone -b v5-lua --single-branch --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
+#git clone -b v5-lua --single-branch --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
 git clone -b lua --single-branch --depth 1 https://github.com/sbwml/luci-app-alist package/alist
 git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git package/lucky
 #添加自定义的软件包源
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages ddns-go
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-ddns-go
+#ddns-go
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages ddns-go
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-ddns-go
+#openclsh
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-openclash
+#aria2
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages aria2
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-aria2
+#docker
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages dockerd
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-docker
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-dockerman
+#FTP
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages vsftpd
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-vsftpd
+
 # Remove packages
 #删除lean库中的插件，使用自定义源中的包。
 rm -rf feeds/packages/net/v2ray-geodata
@@ -53,7 +67,7 @@ rm -rf feeds/luci/applications/luci-app-mosdns
 #sed -i 's/services/vpn/g' package/luci-app-passwall/luasrc/view/Passwall/*.htm
 
 # Default IP
-sed -i 's/192.168.31.1/192.168.31.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.11/192.168.1.12/g' package/base-files/files/bin/config_generate
 
 #修改默认时间格式
 sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $(find ./package/*/autocore/files/ -type f -name "index.htm")
